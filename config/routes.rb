7745fa071a41b1d2cac43a97c0460e17d4579257
bhanu <bhanuprasadmende@gmail.com>
2014-09-04 13:55:54 +0530

    routing error redirect

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     root 'home#about'
   end
   
+  get '*path' => redirect('/')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
