dba090415fea87d59aa3b4734f25240ba24c0905
bhanu <bhanuprasadmende@gmail.com>
2014-09-05 14:59:37 +0530

    assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+require 'carrierwave'
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :update_sanitized_params, if: :devise_controller?
